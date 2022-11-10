192b55a020074d7948b62db3cf075d00b54c7996
egorpugachevskyWork <egorpugachevsky@gmail.com>
2022-11-10 12:22:35 +0300

    CreateInstanceOfClass methods was implemented, however it cannot work without create method.Also user method need to be invoked first

--- MainPart/Faker.cs
+++ MainPart/Faker.cs
@@ -23,7 +23,7 @@ namespace MainPart
 
         //TO DO
         //1. Load all generators from current Assembly +
-        //2. Try to generate value from this generators
+        //2. Try to generate value from this generators+ and try to generate from user generator and from dll methods
         //3. Try to generate not dto with recursion
         //3_a. Forget about loading methods from other dlls
         //3a. First with user generators
@@ -53,6 +53,59 @@ namespace MainPart
         private object GetDefaultValue(Type t)
         {
             return t.IsValueType ? Activator.CreateInstance(t) : null;
+        }
+
+        //find constructors and create object+
+        //fill fields
+        //fill properties
+        private object CreateClass(Type t)
+        {
+            object obj = CreateInstanceOfClass(t);
+            
+        }
+
+        private object CreateInstanceOfClass(Type t)
+        {
+            var infoContructors = t.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly);
+            Array.Sort(infoContructors, (c1, c2) =>
+            {
+                if (c1.GetParameters().Length > c2.GetParameters().Length)
+                    return -1;
+                else if (c1.GetParameters().Length > c2.GetParameters().Length)
+                    return 1;
+                return 0;
+            });
+
+            object obj = null;
+            foreach (var info in infoContructors)
+            {
+                object[] parametrs = GetParametrs(info);
+                obj = info.Invoke(parametrs);
+                if (obj != null)
+                    break;
+            }
+
+            return obj;
+        }
+
+        private object[] GetParametrs(ConstructorInfo info)
+        {
+            if (info.GetParameters().Length == 0)
+                return null;
+
+            object[] parametrs = new object[info.GetParameters().Length];
+            int i = 0;
+            foreach(var param in info.GetParameters())
+            {
+                //user generator first
+                //generator second +
+                //dll generator
+                var foundLibGen = _generators.Find(g => g.CanGenerate(param.ParameterType));
+                parametrs[i] = foundLibGen?.Generate(param.ParameterType, _context) ?? Create(param.ParameterType);
+                i++;
+            }
+
+            return parametrs;
         }
 
         public Faker()
